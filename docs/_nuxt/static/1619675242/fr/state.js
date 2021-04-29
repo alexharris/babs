6f70cd7e2174a9ddebe74f20abcddb5fd71911ec
Alex Harris <alexharris@Alexs-Mac-mini.attlocal.net>
2021-04-29 14:36:40 -0700

    generate

--- docs/_nuxt/static/1619675242/fr/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fbabs\u002F_nuxt\u002Fstatic\u002F1619675242",layout:"default",error:a,state:{region:b,hotspot:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Ffr",config:{app:{basePath:"\u002Fbabs\u002F",assetsPath:"\u002Fbabs\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
--- docs/_nuxt/static/1619675242/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fes\u002Fregion","\u002Ffr\u002Fhotspot","\u002Fru\u002Fregion","\u002Fes","\u002Fko\u002Fhotspot","\u002Ffr\u002Fregion","\u002Fhotspot","\u002Fru","\u002Fko\u002Fregion","\u002Ffr","\u002Fregion","\u002Fes\u002Fhotspot","\u002Fzh\u002Fregion","\u002Fzh","\u002Fko","\u002Fzh\u002Fhotspot","\u002Fru\u002Fhotspot"]})
