46278f40b46c5168181ec1bc5c4e2f57d70b61a3
Alex Harris <alexharris@Alexs-Mac-mini.attlocal.net>
2021-04-28 22:47:34 -0700

    deploy to babs

--- docs/_nuxt/static/1619674845/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fru\u002Fhotspot","\u002Fko\u002Fhotspot","\u002Fzh\u002Fhotspot","\u002Fes\u002Fregion","\u002Ffr","\u002Fru\u002Fregion","\u002Fes\u002Fhotspot","\u002Fko\u002Fregion","\u002Fru","\u002Fes","\u002Ffr\u002Fhotspot","\u002F","\u002Fko","\u002Ffr\u002Fregion","\u002Fzh","\u002Fzh\u002Fregion","\u002Fregion","\u002Fhotspot"]})
--- docs/_nuxt/static/1619674845/zh/region/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619674845",layout:"default",error:a,state:{region:b,hotspot:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fzh\u002Fregion",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
