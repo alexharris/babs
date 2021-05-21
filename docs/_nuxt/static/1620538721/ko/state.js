9fd40fed2b3287cf925d2bb7511e57b9ea7d78b1
alexharris <alexharris6@gmail.com>
2021-05-21 00:04:36 -0700

    build

--- docs/_nuxt/static/1620538721/ko/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fbabs\u002F_nuxt\u002Fstatic\u002F1620538721",layout:"default",error:a,state:{region:b,hotspot:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fko",config:{app:{basePath:"\u002Fbabs\u002F",assetsPath:"\u002Fbabs\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
